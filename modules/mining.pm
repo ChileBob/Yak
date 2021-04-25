042b7bf6a12db3f8e5caa20e527e83a40be284b2
ChileBob <45407466+ChileBob@users.noreply.github.com>
2021-04-24 21:03:53 -0400

    Moved Equihash validator into mining package, tidied

--- modules/mining.pm
+++ modules/mining.pm
@@ -9,13 +9,13 @@
 
 package mining;
 
+require "./blake2b.pm";
+
 use Data::Dumper;
 use Bitcoin::Crypto::Base58 qw(:all);
 use Digest::SHA qw(sha256);
 
 my $debug = 5;						# global debug verbosity, 0 = quiet
-
-							# mining client states
 							
 our $CLIENT_NEW         = 0x00;				# - new connection
 our $CLIENT_SUBSCRIBED  = 0x01;				# - subscribed
@@ -24,43 +24,79 @@ our $CLIENT_IDLE        = 0x10;				# - idle
 our $CLIENT_TARGETED    = 0x11;				# - targetted
 our $CLIENT_ACTIVE      = 0x12;				# - active (mining)
 our $CLIENT_DISCONNECT  = 0xff;				# - disconnected
-	
 
-# Equihash parameters
-#
-# my $N = 192;	# width (bits), integer divisible by (K+1)
-# my $K = 7;	# length of problem
-# 
-# my $strings = 2**($N/($K+1));	# number of strings (16777216)
-# print "strings $strings\n\n";
-# 
-# my $indexes = 2**$K;		# number of indexes (128)
-# print "indexes $indexes\n\n";
-# 
-#solution size = 400 bytes, 3200 bits
-#
-# difficulty check : sha256(sha256(data nonce solution)
-#
 
 #########################################################################################################################################################################
 #
-# check solution matches difficulty target								# NOTE: Solution MUST start with compactSize
+# verify solution matches the required difficulty
 #
-sub check_solution {
+sub verify_difficulty {
 
-	my ($nbits, $header, $nonce, $solution) = @_;							# hex-encoded strings
-	
+	my ($header, $nonce, $solution, $nbits) = @_;							# hex-encoded strings, endian-ness as per block header
+
 	my $target = nbits_to_target($nbits);								# convert nbits to 256-bit target
 	$target =~ s/0*$//;										# strip training zeros
 
 	my $diff = unpack("H*", reverse sha256(sha256(pack("H*", $header . $nonce . $solution)))); 	# get hex-encoded double-sha256 of the block header
 	$diff = substr($diff, 0, length($target));							# cut to length of significant target bytes
 
-	if (hex($diff) <= hex($target) ) {								# check difficulty less or equal to target
-		return(1);
+	if (hex($diff) < hex($target) ) {								# difficulty must be less or equal to target
+		return(1);		
 	}
 }
 
+#########################################################################################################################################################################
+#
+# verify block header & equihash solution 								Based on : https://git.hush.is/hush/hushwebminer/src/branch/master/pool-emu/equihash.pm
+#
+sub verify_equihash {
+
+	my ($header, $nonce, $solution, $N, $K) = @_;							# hex-encoded strings, endian-ness as per block header, equihash N & K integers
+													#
+	my $bpi = ($N / ($K + 1)) + 1;									# bits per index	200/9 = 21       192/7 = 25
+	my $indexes = 2**$K;										# number of indexes	200/9 = 512      192/7 = 128
+	my $hashlen = $N / 4;										# blake2b hash length	200/9 = 50       192/7 = 48
+	my $solsize = ($bpi * $indexes) / 8;								# solution size		200/9 = 1344, 192/7 = 400
+
+	if ( $solsize != readCompactSize(substr($solution, 0, 6))) {					# check solution size
+		return(0);
+	}
+
+	$header   = pack("H*", $header);								# convert args to binary
+	$nonce    = pack("H*", $nonce);
+	$solution = pack("H*", substr($solution, 6));
+
+	my @sol = map { oct "0b$_" } unpack "(a$bpi)*", unpack 'B*', $solution;				# extract indexes from solution
+	@sol == $indexes or return(0);
+									
+	my %uniq; 											# check indexes are unique
+	@uniq{@sol} = (); 
+	keys %uniq == $indexes or return(0);
+
+	for my $step (1..$K) {										# check indexes are ordered
+		my $off = 2**($step-1);
+		$sol[$_] < $sol[$_+$off] or return(0)
+		for map $_*$off*2, 0 .. 2**($K-$step)-1;
+	}
+													# setup blake2b
+	my $blake = blake2b::new ( hashlen  => $hashlen, personal => ('ZcashPoW' . pack 'VV', $N, $K),	)->update ($header . $nonce);
+
+	@sol = map {											# get hashes
+		my $bl = $blake->copy ()->final (pack 'V', $_ / 2);
+		length $bl == $hashlen or return(0);
+		substr $bl, $_ % 2 * ($hashlen/2), ($hashlen/2)	
+	} @sol;
+
+	for my $step (1..$K) {										# XOR hashes
+		@sol = map $sol[$_*2] ^ $sol[$_*2+1], 0 .. @sol/2 - 1;
+		unpack ('B' . $step * 20, $_) =~ /^0+\z/ or return(0) for @sol;
+	}
+	@sol == 1 or return(0);
+
+	if ( hex(unpack("H*", $sol[0])) == 0) {								# valid if final XOR is zero
+		return(1);
+	}
+}
 
 
 #########################################################################################################################################################################
@@ -71,9 +107,9 @@ sub make_coinbase {
 
 	my ($template, $outputs) = @_;
 
-	my @outputs = @{$outputs};	# dereference outputs
+	my @outputs = @{$outputs};									# dereference outputs
 
-	my $coinbase = substr($template->{'coinbasetxn'}->{'data'}, 0, 110);	# transaction header & coinbase input
+	my $coinbase = substr($template->{'coinbasetxn'}->{'data'}, 0, 110);				# transaction header & coinbase input
 
 	my $txn_out = txn_out( $template->{'coinbasetxn'}->{'foundersaddress'}, $template->{'coinbasetxn'}->{'foundersreward'});
 
@@ -82,18 +118,18 @@ sub make_coinbase {
 	my $payout_addr;
 	foreach my $out (@outputs) {
 
-		if (!exists $out->{'percent'}) {				# no percentage, gets remaining block reward
+		if (!exists $out->{'percent'}) {							# no percentage, gets remaining block reward
 			$payout_addr = $out->{'address'};
 		}
-		else {								# fixed percentage, generate output
+		else {											# fixed percentage, generate output
 			my $amount = int (($out->{'percent'} * $reward) / 100);	# reduce remaining reward
 			$txn_out .= txn_out( $out->{'address'}, $amount);
 			$reward -= $amount;
 		}
 	}
-	$txn_out .= txn_out( $payout_addr, $reward);				# last txn is the remaining reward
+	$txn_out .= txn_out( $payout_addr, $reward);							# last txn is the remaining reward
 
-	$txn_out .= '00000000000000000000000000000000000000';			# final parts of txn
+	$txn_out .= '00000000000000000000000000000000000000';						# final parts of txn
 
 	return($coinbase . hexCompactSize(scalar @outputs + 1) . $txn_out);
 }
@@ -105,19 +141,19 @@ sub make_coinbase {
 #
 sub nbits_to_target {
 
-	my ($nbits) = @_;								# hex-encoded nBits
+	my ($nbits) = @_;										# hex-encoded nBits
 
-	my $target = substr($nbits,2,6);						# most significant bits
+	my $target = substr($nbits,2,6);								# most significant bits
 
-	my $exp = hex(substr($nbits,0,2)) - 3;						# exponent
+	my $exp = hex(substr($nbits,0,2)) - 3;								# exponent
 	while ($exp > 0) {								 
 		$target .= '00';
 		$exp--;
 	}
-	while (length($target) < 64) {							# leading zeros
+	while (length($target) < 64) {									# leading zeros
 		$target = '00' . $target;
 	}
-	return($target);								# return target
+	return($target);										# return target
 }
 
 
@@ -127,13 +163,13 @@ sub nbits_to_target {
 #
 sub txn_out {
 
-	my ($address, $zats) = @_;							# payment address, amount in zats
+	my ($address, $zats) = @_;									# payment address, amount in zats
 
 	print "txn_out() : $address, $zats\n";
 
-	my $amount = unpack("H*", reverse pack("H*", sprintf("%016X", $zats)));		# 8-bytes, little-endian
+	my $amount = unpack("H*", reverse pack("H*", sprintf("%016X", $zats)));				# 8-bytes, little-endian
 
-	my $script = addr_to_script($address);						# raw script
+	my $script = addr_to_script($address);								# raw script
 
 	return($amount . $script);
 }
@@ -144,16 +180,16 @@ sub txn_out {
 #
 sub addr_to_script {
 
-	my ($address) = @_;								# payment address
+	my ($address) = @_;										# payment address
 
-	my $payhash = unpack("H*", substr(decode_base58check($address),2));		# raw script
+	my $payhash = unpack("H*", substr(decode_base58check($address),2));				# raw script
 
-	if ( $address =~ m/^.1/) {							# pay to t1/s1
+	if ( $address =~ m/^.1/) {									# pay to t1/s1
 
 		return($payhash = '1976a914' . $payhash . '88ac');
 
 	}
-	elsif ( $address =~ m/^.3/) {							# pay to t3/s3
+	elsif ( $address =~ m/^.3/) {									# pay to t3/s3
 
 		return($payhash = 'a914' . $payhash . '87');
 	}
@@ -166,19 +202,19 @@ sub addr_to_script {
 #
 sub block_reward {
 
-	my ($hexData) = @_;								# coinbase transaction as hex-encoded string
+	my ($hexData) = @_;										# coinbase transaction as hex-encoded string
 
-	my $blockReward = 0;								# block reward
+	my $blockReward = 0;										# block reward
 
-	my $hexOutputs = substr($hexData, 112, -36);					# transaction outputs
+	my $hexOutputs = substr($hexData, 112, -36);							# transaction outputs
 
-	while (length($hexOutputs) > 0) {						# loop through all outputs
+	while (length($hexOutputs) > 0) {								# loop through all outputs
 
-		$blockReward += hex(reverse_bytes(substr($hexOutputs, 0, 16)));		# add value to block reward total
+		$blockReward += hex(reverse_bytes(substr($hexOutputs, 0, 16)));				# add value to block reward total
 		$hexOutputs = substr($hexOutputs, (18 + (2 * readCompactSize(substr($hexOutputs, 16, 10)))));	
 	}
 
-	return($blockReward);								# block reward in zats
+	return($blockReward);										# block reward in zats
 }
 
 
@@ -194,30 +230,30 @@ sub hexCompactSize {
 	my ($hexData, $type) = @_;
 
 	if ($type eq 's') {
-		$length = length($hexData) / 2;									# hex string, convert to length in bytes
+		$length = length($hexData) / 2;								# hex string, convert to length in bytes
 	}
 	elsif ($type eq 't') {
-		$length = (length($hexData) / 2) + 1;								# transaction, hex-string length plus 1
+		$length = (length($hexData) / 2) + 1;							# transaction, hex-string length plus 1
 	}
-	else {													# integer 
+	else {												# integer 
 		$length = $hexData;
 	}
 
-	if ($length < 253) {											# 8-bit
+	if ($length < 253) {										# 8-bit (happens all the time)
 		$compact = sprintf("%02x", $length);
 	        return($compact);
 	}
-	elsif ( ($length >= 253) && ($length <= 65535) ) {							# 16-bit
+	elsif ( ($length >= 253) && ($length <= 65535) ) {						# 16-bit (happens sometimes)
 		$compact = sprintf("%04x", $length);
 		$compact = reverse_bytes($compact);
 		return("fd$compact");
 	}
-	elsif ( ($length > 65556) && ($length <= 4294967295)) {							# 32-bit
+	elsif ( ($length > 65556) && ($length <= 4294967295)) {						# 32-bit (never happened, not likely either)
 		$compact = sprintf("%08x", $length);
 		$compact = reverse_bytes($compact);
 		return("fe$compact");
 	}
-	else {													# 64-bit
+	else {												# 64-bit (you gotta be kidding!! but just for completeness)
 		$compact = sprintf("%016x", $length);
 		$compact = reverse_bytes($compact);
 		return("ff$compact");
@@ -241,7 +277,7 @@ sub readCompactSize {
 	elsif ( substr($hexData,0,2) eq 'ff') {
 		return(hex(reverse_bytes(substr($hexData,2,16))));
 	}
-	else {							# works !
+	else {				
 		return(hex(substr($hexData,0,2)));
 	}
 }
@@ -254,28 +290,28 @@ sub merkleroot {
 
 	my @hashes = ();
 
-	foreach (@{$_[0]}) {											# dereference, data is hex-encoded
-		push @hashes, $_;										# txids are little-endian
+	foreach (@{$_[0]}) {										# dereference, data is hex-encoded
+		push @hashes, $_;									# txids are little-endian
 	}
 
-	if ( (scalar @hashes) == 1 ) {										# if its an empty block (1 tx)
-		return($hashes[0]);										# return coinbase txn hash as merkleroot
+	if ( (scalar @hashes) == 1 ) {									# if its an empty block (1 tx)
+		return($hashes[0]);									# return coinbase txn hash as merkleroot
 	}
 
-	while ((scalar @hashes) > 1) {										# loop through array until there's only one value left
+	while ((scalar @hashes) > 1) {									# loop through array until there's only one value left
 
-		if ( ((scalar @hashes) % 2) != 0 )  {								# duplicate last hash if there's an odd number
+		if ( ((scalar @hashes) % 2) != 0 )  {							# duplicate last hash if there's an odd number
 			push @hashes, $hashes[((scalar @hashes) - 1)];
 		}
 
 		my @joinedHashes;
 
-		while (my @pair = splice @hashes, 0, 2) {							# get a pair of hashes
-			push @joinedHashes, hash_this(reverse_bytes("$pair[1]$pair[0]"));			# get the hash
+		while (my @pair = splice @hashes, 0, 2) {						# get a pair of hashes
+			push @joinedHashes, hash_this(reverse_bytes("$pair[1]$pair[0]"));		# get the hash
 		}
-		@hashes = @joinedHashes;									# replace hashes with joinedHashes
+		@hashes = @joinedHashes;								# replace hashes with joinedHashes
 	}
-	return($hashes[0]);											# returns hex-encoded big-endian
+	return($hashes[0]);										# returns hex-encoded big-endian
 }
 
 
@@ -285,9 +321,9 @@ sub merkleroot {
 #
 sub hash_this {						
 
-	my ($hexData) = @_;											# hex-encoded transaction data
+	my ($hexData) = @_;										# hex-encoded transaction data
 
-	return(unpack("H*", reverse sha256(sha256(pack "H*", $hexData))));					# returns hash, hex-encoded, little-endian
+	return(unpack("H*", reverse sha256(sha256(pack "H*", $hexData))));				# returns hash, hex-encoded, little-endian
 }
 
 
@@ -295,7 +331,7 @@ sub hash_this {
 #
 # reverse byte order of hex-encoded string
 #
-sub reverse_bytes {												# reverse byte order of a hex-encoded string
+sub reverse_bytes {											# reverse byte order of a hex-encoded string
 
 	my ($hexString) = @_;
 
